024c2dfd3df3cade3691d202828f9aaf34564e18
HolstAndreas <166335551+HolstAndreas@users.noreply.github.com>
2025-03-12 19:33:45 +0200

    Initial commit: initial flightplanner setup

---
+++ backend/src/main/java/com/holstandreas/srv/FlightplannerApplication.java
@@ -0,0 +1,13 @@
+package com.holstandreas.srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightplannerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlightplannerApplication.class, args);
+  }
+
+}
---
+++ backend/src/test/java/com/holstandreas/srv/FlightplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holstandreas.srv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightplannerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
