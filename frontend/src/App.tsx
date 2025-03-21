a8ea0d62cdaddaed9cfd3be09cb1793a8c3eadf3
HolstAndreas <166335551+HolstAndreas@users.noreply.github.com>
2025-03-21 20:34:41 +0200

    refactor(filtering): refactor to use new filtering logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import FilteringSection from './components/FilteringSection'
 import FlightSection from './components/FlightSection'
 
 const App = () => {
+  const [filters, setFilters] = useState<Record<string, any>>({});
+
+  const handleFiltersChange = (newFilters: Record<string, any>) => {
+    setFilters(newFilters);
+  }
+
   return (
-    <div className="flex min-h-screen w-full">
-      <div className="my-8 mx-40 flex-1 bg-green-100 p-4 shadow-md">
-        <FilteringSection />
+    <div className="flex flex-col h-screen w-full">
+      <div>
+        <FilteringSection onFiltersChange={handleFiltersChange}>
+        </FilteringSection>
+      </div>
+      <div className="mb-8 mx-8 flex-1 bg-green-100 p-4 shadow-md">
         <FlightSection />
       </div>
     </div>
--- frontend/src/components/FilteringSection.tsx
+++ frontend/src/components/FilteringSection.tsx
@@ -1,7 +1,15 @@
-const FilteringSection = () => {
-  return (
-    <div>FilteringSection</div>
-  )
+import FlightsFilter from "./FlightsFilter";
+
+interface FilteringSectionProps {
+  children?: React.ReactNode;
+  onFiltersChange: (filters: Record<string, any>) => void;
 }
 
-export default FilteringSection
+export default function FilteringSection({ children, onFiltersChange }: FilteringSectionProps) {
+  return (
+    <div className="w-full h-100 bg-amber-100 border-r border-amber-200 p-4 md:block">
+      <FlightsFilter onFiltersChange={onFiltersChange} />
+      {children}
+    </div>
+  )
+}
