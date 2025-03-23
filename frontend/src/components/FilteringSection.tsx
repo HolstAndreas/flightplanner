761a0191492447681a088af4d2da356ac220aaa4
HolstAndreas <166335551+HolstAndreas@users.noreply.github.com>
2025-03-23 20:00:19 +0200

    refactor(FilteringSection): add proper prop drilling

--- frontend/src/components/FilteringSection.tsx
+++ frontend/src/components/FilteringSection.tsx
@@ -1,14 +1,21 @@
+import { SeatRecommendationParameters } from "../types";
 import FlightsFilter from "./FlightsFilter";
 
 interface FilteringSectionProps {
   children?: React.ReactNode;
   onFiltersChange: (filters: Record<string, any>) => void;
+  seatPreferences: SeatRecommendationParameters;
+  onRecommendationsChange: (seatPreferences: SeatRecommendationParameters) => void;
 }
 
-export default function FilteringSection({ children, onFiltersChange }: FilteringSectionProps) {
+export default function FilteringSection({ children, onFiltersChange, onRecommendationsChange, seatPreferences }: FilteringSectionProps) {
   return (
     <div className="w-full border-r border-amber-200 p-4 md:block">
-      <FlightsFilter onFiltersChange={onFiltersChange} />
+      <FlightsFilter 
+        onFiltersChange={onFiltersChange}
+        seatPreferences={seatPreferences}
+        onRecommendationsChange={onRecommendationsChange}
+      />
       {children}
     </div>
   )
