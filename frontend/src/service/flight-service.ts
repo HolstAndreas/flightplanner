1e00896bc45b3f0e79679a0bff81bf7111d1dd8f
HolstAndreas <166335551+HolstAndreas@users.noreply.github.com>
2025-03-23 10:19:07 +0200

    feat(flight-service): endpoints for filters and flight retrieval

---
+++ frontend/src/service/flight-service.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { FlightResponse, LocationResponse } from "../types";
+
+const API_URL = '/api';
+
+interface FlightFilters {
+  departureAirport?: string,
+  departureCountry?: string,
+  departureCity?: string,
+  arrivalAirport?: string,
+  arrivalCountry?: string,
+  arrivalCity?: string,
+  startingDate?: string,
+  endingDate?: string,
+  duration?: string;
+  maxPrice?: string;
+  sortColumn?: string;
+  sortDirection?: string;
+}
+
+export const fetchFlights = async (filters?: FlightFilters): Promise<FlightResponse> => {
+  try {
+    const response = await axios.get(`${API_URL}/flights`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      params: filters
+    });
+
+    return response.data;
+
+  } catch (error) {
+    console.error('Failed to fetch flights: ', error);
+    throw error;
+  }
+}
+
+export const fetchLocations = async (): Promise<LocationResponse> => {
+  try {
+    const response = await axios.get(`${API_URL}/airports`, {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Failed to fetch location filters ', error);
+    throw error;
+  }
+}
