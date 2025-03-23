b299b373a18635c4f61508307563b6b4cd5d4189
HolstAndreas <166335551+HolstAndreas@users.noreply.github.com>
2025-03-23 19:57:11 +0200

    refactor(App): add SeatPreferences state and management to app. Add Props to pass down

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,41 @@
 import { useState } from 'react';
 import FilteringSection from './components/FilteringSection'
 import FlightSection from './components/FlightSection'
+import { SeatRecommendationParameters } from './types';
 
 const App = () => {
   const [filters, setFilters] = useState<Record<string, any>>({});
+  const [seatPreferences, setSeatPreferences] = useState<SeatRecommendationParameters>({
+    hasWindow: false,
+    hasLegspace: false,
+    hasExit: false,
+    seatsAmount: 1,
+    travelClass: "TOURIST"
+  });
 
   const handleFiltersChange = (newFilters: Record<string, any>) => {
     setFilters(newFilters);
   }
 
+  const handleRecommendationChange = (newSeatPreferences: SeatRecommendationParameters) => {
+    setSeatPreferences(newSeatPreferences);
+  }
+
   return (
     <div className="flex flex-col w-full">
       <div>
-        <FilteringSection onFiltersChange={handleFiltersChange}>
+        <FilteringSection 
+          onFiltersChange={handleFiltersChange}
+          onRecommendationsChange={handleRecommendationChange}
+          seatPreferences={seatPreferences}
+        >
         </FilteringSection>
       </div>
       <div className="mb-8 mx-8 flex-1 bg-green-100 p-4 shadow-md">
-        <FlightSection filters={filters}/>
+        <FlightSection 
+          filters={filters}
+          seatParameters={seatPreferences}
+        />
       </div>
     </div>
   );
